80d90bc53d3711c141142c8f5030825986c8a152
unknown <gregmasterbr@gmail.com>
2022-03-13 23:25:08 -0300

    Tela de inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -2,6 +2,8 @@ import imp
 import django
 from django.shortcuts import render
 from django.http import HttpResponse
+from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
-   return render(request,'subscriptions/subscription_form.html') 
+   context = {'form':SubscriptionForm()}
+   return render(request,'subscriptions/subscription_form.html',context) 
