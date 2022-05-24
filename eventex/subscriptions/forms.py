dbc875a64c378d26a6030005d40ceef21d5592d5
unknown <gregmasterbr@gmail.com>
2022-05-23 21:11:51 -0300

    Função para validar o CPF - Se é apenas número ou se tem 11 digitos

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,8 +1,15 @@
-import email
+from django.core.exceptions import ValidationError
 from django import forms
+
+def validate_cpf(value):
+    if not value.isdigit():
+        raise forms.ValidationError('CPF deve conter apenas números','digits')
+
+    if len(value) != 11:
+        raise forms.ValidationError('CPF deve conter 11 números','length')
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='E-mail')
     phone = forms.CharField(label='Telefone')
