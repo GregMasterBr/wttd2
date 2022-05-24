96e7b772cb43839a72bfb1293ef01276f76df632
unknown <gregmasterbr@gmail.com>
2022-05-23 21:59:30 -0300

    Permitindo que Email e Telefone seja opcional porém um deles precisa ser informado.

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -43,6 +43,21 @@ class SubscribeFormTest(TestCase):
         form = self.make_validated_form(name='GREGORIO queiroz')
         self.assertEqual('Gregorio Queiroz', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
     def make_validated_form(self,**kwargs):
         valid = dict(name="Gregorio Queiroz", cpf="12345678901",email="gregmasterbr@gmail.com",phone="15-981057742")
         data = dict(valid,**kwargs)
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -99,6 +99,15 @@ class SubscriptionsNewPostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
+    
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name="Gregorio Queiroz", cpf="12345678901")
+
+        response = self.client.post(r('subscriptions:new'),invalid_data)
+
+        self.assertContains(response,'<ul class="errorlist nonfield">')
+
 
 # @unittest.skip('To be removed.')
 # class SubscribeSucessMessage(TestCase):
@@ -107,3 +116,5 @@ class SubscriptionsNewPostInvalid(TestCase):
 #                     email='gregmasterbr@gmail.com', phone='15-98105-7742')
 #         response = self.client.post('/inscricao/', data, follow=True)
 #         self.assertContains(response, 'Inscrição realizada com sucesso!')
+
+
