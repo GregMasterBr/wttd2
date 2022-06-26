b5d96877539343354e4e472011c0402b3058aded
unknown <gregmasterbr@gmail.com>
2022-06-26 18:43:35 -0300

    Refatorando a view do Subscription -> Removendo as declarações hardcoded. -> Criado um template_name para receber o nome do template. -> Simplifica o método do render, criando uma função para ele. -> Cria uma variavel para SubscriptionForm -> E uma função para pegar o formulário e instanciar caso seja do method POST. -> Ao invés de usar somente a View, usar o TemplateResponseMixin. -> Com o templateresponsemixin, pode remover a implementação do render_to_response. -> Reduzir a responsabilidade do Post isolando as implementações, ficando apenas com as articulações. -> Implementar o get_absolute_url (model, view e test)

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.conf import settings
 from django.shortcuts import render, resolve_url as r
 from django.http import HttpResponseRedirect
@@ -7,18 +8,45 @@ from django.core import mail
 from django.template.loader import render_to_string
 from eventex.subscriptions.models import Subscription
 from django.views.generic import DetailView
+from django.views.generic.base import TemplateResponseMixin
 
-class SubscriptionCreate(View):
+class SubscriptionCreate(TemplateResponseMixin, View):
+   template_name = 'subscriptions/subscription_form.html'
+   form_class = SubscriptionForm
+
    def get(self, *args, **kwargs): # será o empty
-      return render(self.request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
+      return self.render_to_response(self.get_context_data())
 
-   def post(self, *args, **kwargs): # será o create
+   # def post(self, *args, **kwargs): # será o create
       
-      form = SubscriptionForm(self.request.POST)
+   #    form = SubscriptionForm(self.request.POST)
+
+   #    if not form.is_valid():
+   #       return render(self.request, self.template_name,{'form':form})
+      
+   #    subscription = form.save()
+
+   #    #Send subscription email
+   #    _send_email('Confirmação de inscrição',
+   #                settings.DEFAULT_FROM_EMAIL,
+   #                subscription.email,
+   #                'subscriptions/subscription_email.txt',
+   #                {'subscription': subscription})  
+            
+   #    return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))   
+
+   def post (self, *args, **kwargs): # será o create
+      
+      form = self.get_form()
 
       if not form.is_valid():
-         return render(self.request,'subscriptions/subscription_form.html',{'form':form})
+         return self.form_invalid(form)
+      return self.form_valid(form)
+
+   def form_invalid(self, form):
+      return self.render_to_response(self.get_context_data(form=form))
       
+   def form_valid(self, form):      
       subscription = form.save()
 
       #Send subscription email
@@ -28,7 +56,17 @@ class SubscriptionCreate(View):
                   'subscriptions/subscription_email.txt',
                   {'subscription': subscription})  
             
-      return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))   
+      return HttpResponseRedirect(subscription.get_absolute_url()) 
+
+   def get_form(self):
+      if self.request.method == 'POST':
+         return self.form_class(self.request.POST)
+      return self.form_class()
+
+   def get_context_data(self, **kwargs):
+      context = dict(kwargs)
+      context.setdefault('form', self.get_form())
+      return context 
 
 new =  SubscriptionCreate.as_view()
 
