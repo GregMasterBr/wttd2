1d9c966d68bc93fd66b21527f954570cd8b76508
unknown <gregmasterbr@gmail.com>
2022-06-26 18:03:06 -0300

    Removendo os comentários dos experimentos anteriores durante a refatoração e melhoria da view da subscription.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -8,38 +8,6 @@ from django.template.loader import render_to_string
 from eventex.subscriptions.models import Subscription
 from django.views.generic import DetailView
 
-# def new(request):
-#    if request.method == 'POST':
-#       return create(request)
-      
-#    return empty_form(request)
-
-# def empty_form(request):
-#    return render(request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
-
-# def create(request):
-#    form = SubscriptionForm(request.POST)
-
-#    if not form.is_valid():
-#       return render(request,'subscriptions/subscription_form.html',{'form':form})
-
-#    #subscription = Subscription.objects.create(**form.cleaned_data)
-#    subscription = form.save()
-
-#    #Send subscription email
-
-#    _send_email('Confirmação de inscrição',
-#                settings.DEFAULT_FROM_EMAIL,
-#                subscription.email,
-#                'subscriptions/subscription_email.txt',
-#                {'subscription': subscription})  
-#    #Save in DB
-
-#    # Success feddback - Não precisa mais devido a tela de agradecimento.
-#    #messages.success(request,'Inscrição realizada com sucesso!')
-         
-#    return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))
-
 class SubscriptionCreate(View):
    def get(self, *args, **kwargs): # será o empty
       return render(self.request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
