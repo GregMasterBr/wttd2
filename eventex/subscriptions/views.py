2f8cd7d50e4021fc2bbaf06903989095832f75af
unknown <gregmasterbr@gmail.com>
2022-06-26 19:28:23 -0300

    Refatorando a view do Subscription -> Extrair o SENDMAIl -> Criar uma função para o sendemail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -14,22 +14,22 @@ class SubscriptionCreate(CreateView):
 
    def form_valid(self, form):      
       response = super().form_valid(form)
-
-      #Send subscription email
-      _send_email('Confirmação de inscrição',
-                  settings.DEFAULT_FROM_EMAIL,
-                  self.object.email,
-                  'subscriptions/subscription_email.txt',
-                  {'subscription': self.object})  
-            
+      self.send_email()
       return response
-   
+
+   def send_email(self):
+      #Send subscription email
+      subject = 'Confirmação de inscrição'
+      from_ = settings.DEFAULT_FROM_EMAIL
+      to_ = self.object.email 
+      template_name = 'subscriptions/subscription_email.txt'
+      context = {'subscription': self.object}
+
+      body = render_to_string(template_name,context)
+      return mail.send_mail(subject,body,from_,[from_, to_])            
+
 new =  SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
-
-def _send_email(subject, from_,to_,template_name,context):
-   body = render_to_string(template_name,context)
-   mail.send_mail(subject,body,from_,[from_, to_])
 
 
