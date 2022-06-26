2ff22f925c4d4bfba313be3a6d56457c30b1854e
unknown <gregmasterbr@gmail.com>
2022-06-26 16:34:28 -0300

    Refatorando a view Talk List. -> Ajusta o PeriodManager do manager.py -> Transforma o PeriodManager em PeriodQuerySet. Vou passar o PeriodManager através de um modo especial atribuindo a essa variavel um models.Manager.from_queryset(PeriodQuerySet).

--- eventex/core/managers.py
+++ eventex/core/managers.py
@@ -33,10 +33,20 @@ class KindContactManager(models.Manager):
         return self.get_queryset().phones()
 
 
-class PeriodManager(models.Manager):
+# class PeriodManager(models.Manager):
+#     MIDDAY = '12:00'
+#     def at_morning(self):
+#         return self.filter(start__lt=self.MIDDAY)
+
+#     def at_afternoon(self):
+#         return self.filter(start__gte=self.MIDDAY)        
+
+class PeriodQuerySet(models.QuerySet):
     MIDDAY = '12:00'
     def at_morning(self):
         return self.filter(start__lt=self.MIDDAY)
 
     def at_afternoon(self):
-        return self.filter(start__gte=self.MIDDAY)        
+        return self.filter(start__gte=self.MIDDAY)                
+
+PeriodManager = models.Manager.from_queryset(PeriodQuerySet)
