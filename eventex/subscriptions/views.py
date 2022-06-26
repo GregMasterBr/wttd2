ed2b495d2bad65c85b4ea6ff2590f43b70b82460
unknown <gregmasterbr@gmail.com>
2022-06-26 20:14:40 -0300

    EmailCreateMixin -> Separou toda a capacidade de enviar email para um mixin. E o Form herda esse mixin. -> EmailCreateView -> Renomeado o SubscriptionCreate para EmailCreateView -> Remove a indicação da model da subscription e do form -> Criado um arquivo mixins e adicionado toda a lógica do email para esse arquivo. Importa o EmailCreateView para a view do subscribe.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,62 +1,13 @@
-from django.conf import settings
-from django.shortcuts import render, resolve_url as r
-from django.http import HttpResponseRedirect
-from django.views.generic import View
 from eventex.subscriptions.forms import SubscriptionForm
-from django.core import mail
-from django.template.loader import render_to_string
 from eventex.subscriptions.models import Subscription
-from django.views.generic import CreateView, DetailView
+from django.views.generic import DetailView
+from eventex.subscriptions.mixins import EmailCreateView
 
-class SubscriptionCreate(CreateView):
-   model = Subscription
-   form_class = SubscriptionForm
-   email_to = None
-   email_context_name  = None
-   email_template_name = None 
-   email_from = settings.DEFAULT_FROM_EMAIL
+new =  EmailCreateView.as_view(
+   model = Subscription,
+   form_class = SubscriptionForm,   
    email_subject = 'Confirmação de inscrição'
-
-   def form_valid(self, form):      
-      response = super().form_valid(form)
-      self.send_email()
-      return response
-
-   def send_email(self):
-      #Send subscription email
-      subject = self.email_subject
-      from_ =  self.email_from
-      to_ = self.get_email_to() 
-      template_name = self.get_email_template_name()
-      context = self.get_email_context_data()
-
-      body = render_to_string(template_name,context)
-      return mail.send_mail(subject,body,from_,[from_, to_])            
-
-   def get_email_template_name(self):
-      if self.email_template_name:
-         return self.email_template_name
-      
-      meta = self.object._meta
-      return '{}/{}_email.txt'.format(meta.app_label, meta.model_name)
-
-
-   def get_email_context_data(self, **kwargs):
-      context = dict(kwargs)
-      context.setdefault(self.get_email_context_name(), self.object)
-      return context
-
-   def get_email_context_name(self):
-      if self.email_context_name:
-         return self.email_context_name
-      return self.object._meta.model_name
-
-   def get_email_to(self):
-      if self.email_to:
-         return self.email_to
-      return self.object.email
-
-new =  SubscriptionCreate.as_view()
+)
 
 detail = DetailView.as_view(model=Subscription)
 
