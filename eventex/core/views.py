489056c04e8d576c37125fca9df3dabb7a0da7b7
unknown <gregmasterbr@gmail.com>
2022-06-26 17:42:29 -0300

    Removendo os comentários dos experimentos durante a refatoração e melhoria da view Detail View da core

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,22 +1,7 @@
-from msilib.schema import ListView
-from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
 from eventex.core.models import Course, Speaker, Talk
 
 home = ListView.as_view(template_name='index.html', model = Speaker)
-
-# def speaker_detail(request, slug):
-#     # speaker = Speaker(
-#     #         name = 'Grace Hopper',
-#     #         slug = 'grace-hopper',
-#     #         title = 'Programadora e almirante',
-#     #         photo = 'https://hbn.link/hopper-pic',
-#     #         website = 'https://hbn.link/hopper-site',
-#     #         description = 'Inventora do compilador, criadora da linguagem de programação Flow-Matic. A Linguagem serviu de base para a linguagem COBOL permitindo a popularização das aplicações comerciais.'
-#     #     )    
-#     #speaker = Speaker.objects.get(slug=slug)
-#     speaker = get_object_or_404(Speaker,slug=slug)
-#     return render(request,'core/speaker_detail.html',{'speaker':speaker}) 
 
 speaker_detail = DetailView.as_view(model=Speaker)
 
