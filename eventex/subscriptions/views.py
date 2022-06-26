76415cdf9128c8ccb9b4255b159384acc82d4afa
unknown <gregmasterbr@gmail.com>
2022-06-26 18:02:22 -0300

    Criando o SubscriptionCreate para substituir os métodos new, emppty_form e o create. -> Cria uma classe e herda de View. -> Remove o New, que fazia apenas o despatch para get ou post.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,45 +1,68 @@
 from django.conf import settings
 from django.shortcuts import render, resolve_url as r
 from django.http import HttpResponseRedirect
+from django.views.generic import View
 from eventex.subscriptions.forms import SubscriptionForm
 from django.core import mail
 from django.template.loader import render_to_string
 from eventex.subscriptions.models import Subscription
 from django.views.generic import DetailView
 
-def new(request):
-   if request.method == 'POST':
-      return create(request)
+# def new(request):
+#    if request.method == 'POST':
+#       return create(request)
       
-   return empty_form(request)
+#    return empty_form(request)
 
+# def empty_form(request):
+#    return render(request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
 
-def empty_form(request):
-   return render(request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
+# def create(request):
+#    form = SubscriptionForm(request.POST)
 
+#    if not form.is_valid():
+#       return render(request,'subscriptions/subscription_form.html',{'form':form})
 
-def create(request):
-   form = SubscriptionForm(request.POST)
+#    #subscription = Subscription.objects.create(**form.cleaned_data)
+#    subscription = form.save()
 
-   if not form.is_valid():
-      return render(request,'subscriptions/subscription_form.html',{'form':form})
+#    #Send subscription email
 
-   #subscription = Subscription.objects.create(**form.cleaned_data)
-   subscription = form.save()
+#    _send_email('Confirmação de inscrição',
+#                settings.DEFAULT_FROM_EMAIL,
+#                subscription.email,
+#                'subscriptions/subscription_email.txt',
+#                {'subscription': subscription})  
+#    #Save in DB
 
-   #Send subscription email
-
-   _send_email('Confirmação de inscrição',
-               settings.DEFAULT_FROM_EMAIL,
-               subscription.email,
-               'subscriptions/subscription_email.txt',
-               {'subscription': subscription})  
-   #Save in DB
-
-   # Success feddback - Não precisa mais devido a tela de agradecimento.
-   #messages.success(request,'Inscrição realizada com sucesso!')
+#    # Success feddback - Não precisa mais devido a tela de agradecimento.
+#    #messages.success(request,'Inscrição realizada com sucesso!')
          
-   return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))
+#    return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))
+
+class SubscriptionCreate(View):
+   def get(self, *args, **kwargs): # será o empty
+      return render(self.request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
+
+   def post(self, *args, **kwargs): # será o create
+      
+      form = SubscriptionForm(self.request.POST)
+
+      if not form.is_valid():
+         return render(self.request,'subscriptions/subscription_form.html',{'form':form})
+      
+      subscription = form.save()
+
+      #Send subscription email
+      _send_email('Confirmação de inscrição',
+                  settings.DEFAULT_FROM_EMAIL,
+                  subscription.email,
+                  'subscriptions/subscription_email.txt',
+                  {'subscription': subscription})  
+            
+      return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))   
+
+new =  SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
 
